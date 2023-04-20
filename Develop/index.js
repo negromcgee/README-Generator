70abf60c97d59dac7ce9611f03b6a1ef07332305
Dior G <diorgoolsby@Hotmail.com>
2023-04-20 13:48:05 -0700

    creating the function

--- Develop/index.js
+++ Develop/index.js
@@ -2,11 +2,40 @@
 const inq = require('inquirer');
 const generateMarkdown = require('./generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+ {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+ },
+
+{
+    type: 'input',
+    name: 'github',
+    message: 'What is your Github username?',
+},
+
+{
+    type: 'input',
+    name: 'email',
+    message: 'What is your email address?',
+},
+
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeReadmeFile(README, , filename = 'README.md') {
+    const content = `# ${title}
+  
+  ${description}`;
+  
+    fs.writeFile(filename, content, (err) => {
+      if (err) throw err;
+      console.log(`${filename} has been created successfully.`);
+    });
+  }
 // TODO: Create a function to initialize app
 function init() {}
 
