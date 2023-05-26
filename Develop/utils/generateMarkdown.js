0c11898347caac7c5cb414d38915089dfb4f23fe
Dior G <diorgoolsby@Hotmail.com>
2023-05-26 14:39:47 -0700

    updating all

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,17 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if license == 
-}
+
+ 
+};
 
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+   if license == "MIT":
+      return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  if license == "Apache":
+      return "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  if license == "GPLv3":
+      return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+  if license == "BSD":
+      return "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+  if license == "":
+      return ""
+  else:
+      return "Unknown license"
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  def extract_license_section(readme_contents):
+    license_section_start = readme_contents.find("## License")
+    if license_section_start == -1:
+        return ""
+    license_section_end = readme_contents.find("\n##", license_section_start)
+    if license_section_end == -1:
+        license_section_end = len(readme_contents)
+    return readme_contents[license_section_start:license_section_end]
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -34,11 +58,7 @@ function generateMarkdown(data) {
   ## Usage
   ${data.usage}
 
-  ## License
-  ![License](https://img.shields.io/badge/License-${data.license}-blue.svg)
-
   ## Contributions
-
   ${data.contribution}
 
   ## Tests
@@ -48,9 +68,6 @@ function generateMarkdown(data) {
   - ${data.email}
   - Github.com/${data.username}
 `;
-}
-
-`;
-}
+};
 
 module.exports = generateMarkdown;
